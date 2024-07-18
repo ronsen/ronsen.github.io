a39525c6f5c7234bf8c7a1ca9306bb386622fa13
Ronsen <ronsen@startrek.or.id>
2024-07-18 21:29:31 +0700

    update

--- src/lib/components/socials.svelte
+++ src/lib/components/socials.svelte
@@ -11,8 +11,8 @@
 
 <div class="flex justify-center gap-5 text-lg mt-6">
 	<a href="mailto:ronsen@startrek.or.id" class="hover:text-zinc-50"><Fa icon={faEnvelope} /></a>
-	<a href="https://arsak.web.id/" class="text-amber-600 hover:text-amber-400"><Fa icon={faBlogger} /></a>
+	<a href="https://sintaks.web.id/" class="text-amber-600 hover:text-amber-400"><Fa icon={faBlogger} /></a>
 	<a href="https://github.com/ronsen" class="hover:text-zinc-50"><Fa icon={faGithub} /></a>
 	<a href="https://www.linkedin.com/in/ronaldnababan" class="hover:text-zinc-50"><Fa icon={faLinkedinIn} /></a>
-	<a href="https://twitter.com/rnldnbbn" class="hover:text-zinc-50"><Fa icon={faTwitter} /></a>
+	<a href="https://x.com/rnldnbbn" class="hover:text-zinc-50"><Fa icon={faTwitter} /></a>
 </div>
