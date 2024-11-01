08547c3eb6df17bb0ec5479ad24d122c3be8cbfe
Ronsen <ronsen@startrek.or.id>
2024-11-01 19:33:29 +0700

    upgrade: svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
 	import "../app.css";
+
+	let { children } = $props();
 </script>
 
 <main class="container mx-auto md:w-[720px] m-12 px-8">
-	<slot />
+	{@render children()}
 </main>
