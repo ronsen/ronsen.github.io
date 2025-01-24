73691e5e95861dcbf5f5e0b18243cf04a74ba1a2
Ronsen <ronsen@startrek.or.id>
2025-01-24 19:49:41 +0700

    update

---
+++ docs/_app/immutable/entry/start.DEs1Nxhp.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/uUKC6sht.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,14 +18,15 @@
 >
 
 <section class="text-center">
-	<h1 class="title text-4xl font-semibold mb-3 text-zinc-100 rock-salt">
+	<h1 class="title text-4xl font-semibold mb-6 text-zinc-100 rock-salt">
 		<a href="/">Ronald Nababan</a>
 	</h1>
 
 	<div class="mb-6">
-		Saya seorang programmer lepas yang memiliki pengalaman luas dalam pembuatan
-		perangkat lunak, situs web, dan aplikasi mobile. Selain itu, saya juga
-		memiliki minat besar pada teknologi, fotografi, dan videografi.
+		I reside in Pematangsiantar, Indonesia and work as a freelance programmer
+		with extensive experience in creating software, websites, and mobile
+		applications. Additionally, I am passionate about technology, photography,
+		and videography.
 	</div>
 
 	<div class="flex justify-center mb-6">
@@ -33,7 +34,7 @@
 			href="https://wa.me/6281319479396"
 			class="w-full text-xl font-bold border border-green-600 hover:border-green-700 rounded-full p-4 text-center bg-green-600 hover:bg-green-700 text-zinc-100 hover:text-zinc-50 transition ease-out duration-300"
 		>
-			Ngobrol Dulu, Yuk!
+			Chat with me!
 		</a>
 	</div>
 </section>
