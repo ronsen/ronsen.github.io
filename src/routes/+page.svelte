437226d073f492fad979b3f5c8d26b46ff3c5aa3
Ronsen <ronsen@startrek.or.id>
2025-06-05 14:37:08 +0700

    new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,181 @@
 <script lang="ts">
-	import Portfolios from "$lib/components/portfolios.svelte";
-	import Skills from "$lib/components/skills.svelte";
-	import Socials from "$lib/components/socials.svelte";
-	import Writings from "$lib/components/writings.svelte";
+	import {
+		Code,
+		Database,
+		Frame,
+		Globe,
+		Mail,
+		MessageCircle,
+		Rss,
+		Smartphone,
+		SquareArrowOutUpRight,
+	} from "lucide-svelte";
 </script>
 
 <svelte:head>
 	<title>Ronald Nababan</title>
 </svelte:head>
 
-<div class="mb-6">
+<section class="bg-sky-800 text-white text-center">
 	<a href="/"
 		><img
 			src="./avatar.jpg"
 			alt="[]"
-			class="w-48 mx-auto rounded-full shadow-lg"
+			class="w-48 mx-auto rounded-full shadow"
 		/></a
 	>
-</div>
-
-<section class="text-center">
-	<h1 class="title text-4xl font-semibold mb-6 text-zinc-100">
-		<a href="/">Ronald Nababan</a>
-	</h1>
-
-	<div class="mb-6">
-		I reside in Pematangsiantar, Indonesia and work as a freelance programmer
-		with extensive experience in creating software, websites, and mobile
-		applications. Additionally, I am passionate about technology, photography,
-		and videography.
-	</div>
-
-	<div class="flex justify-center mb-6">
-		<a
-			href="https://wa.me/6281319479396"
-			class="w-full text-xl font-bold border border-green-600 hover:border-green-700 rounded-full p-4 text-center bg-green-600 hover:bg-green-700 text-zinc-100 hover:text-zinc-50 transition ease-out duration-300"
-		>
-			Chat with me!
-		</a>
+	<div class="mt-6">
+		<h1 class="font-bold text-3xl mb-4">Ronald Nababan</h1>
+		<p>Freelance Programmer</p>
 	</div>
 </section>
 
-<Portfolios />
+<section>
+	<h3 class="section-title">Area of Expertise</h3>
+	<div class="grid md:grid-cols-2 gap-4">
+		<div class="card">
+			<div class="card-title">
+				<Globe />
+				<span>Web Programming</span>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-title">
+				<Smartphone />
+				<span>Mobile Programming</span>
+			</div>
+		</div>
+	</div>
+</section>
 
-<Skills />
+<section class="bg-zinc-50">
+	<h3 class="section-title">Technical Skills</h3>
+	<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+		<div class="card">
+			<div class="card-title">
+				<Code size={16} />
+				<span>Programming</span>
+			</div>
+			<div class="card-content">
+				<ul>
+					<li>PHP</li>
+					<li>JavaScript</li>
+					<li>Python</li>
+					<li>Java</li>
+					<li>Dart</li>
+				</ul>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-title">
+				<Frame size={16} />
+				<span>Frameworks</span>
+			</div>
+			<div class="card-content">
+				<ul>
+					<li>Laravel</li>
+					<li>Svelte/SvelteKit</li>
+					<li>Prisma</li>
+					<li>ExpressJS</li>
+					<li>Flutter</li>
+				</ul>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-title">
+				<Database size={16} />
+				<span>Databases</span>
+			</div>
+			<div class="card-content">
+				<ul>
+					<li>PostgreSQL</li>
+					<li>MariaDB/MySQL</li>
+					<li>sqlite</li>
+				</ul>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-title">
+				<Smartphone size={16} />
+				<span>Tools</span>
+			</div>
+			<div class="card-content">
+				<ul>
+					<li>Linux</li>
+					<li>Docker</li>
+					<li>git</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
 
-<Writings />
+<section>
+	<h3 class="section-title">Projects</h3>
+	<div class="grid md:grid-cols-3 gap-4">
+		<div class="card">
+			<div class="flex justify-between items-center gap-2">
+				<div class="inline-flex items-center gap-1">
+					<img
+						src="https://www.hetanews.com/favicon.ico"
+						width="16px"
+						alt="[]"
+					/>
+					<span>Heta News</span>
+				</div>
+				<a href="https://www.hetanews.com" target="_blank"
+					><SquareArrowOutUpRight size={14} /></a
+				>
+			</div>
+		</div>
+		<div class="card">
+			<div class="flex justify-between items-center gap-2">
+				<div class="inline-flex items-center gap-1">
+					<img src="https://fokus24.id/favicon.ico" width="16px" alt="[]" />
+					<span>Fokus24</span>
+				</div>
+				<a href="https://fokus24.id" target="_blank"
+					><SquareArrowOutUpRight size={14} /></a
+				>
+			</div>
+		</div>
+		<div class="card">
+			<div class="flex justify-between items-center gap-2">
+				<div class="inline-flex items-center gap-1">
+					<img src="https://kilasutama.com/favicon.ico" width="16px" alt="[]" />
+					<span>Kilas Utama</span>
+				</div>
+				<a href="https://kilasutama.com" target="_blank"
+					><SquareArrowOutUpRight size={14} /></a
+				>
+			</div>
+		</div>
+	</div>
+</section>
 
-<Socials />
+<section class="bg-zinc-50">
+	<h3 class="section-title">Let's Connect</h3>
+	<div class="text-center mb-6">
+		Do you have any questions regarding website and application development?
+		Let's discuss first!
+	</div>
+	<div class="flex justify-center items-center gap-4">
+		<a
+			href="mailto:ronsen@startrek.or.id"
+			class="btn btn-primary inline-flex items-center gap-2"
+			><Mail /><span>Email me</span></a
+		>
+		<a
+			href="mailto:ronsen@startrek.or.id"
+			class="btn btn-success inline-flex items-center gap-2"
+			><MessageCircle /><span>WhatsApp</span></a
+		>
+	</div>
+</section>
+
+<section class="bg-zinc-900 text-white">
+	<div class="flex justify-center items-center gap-2">
+		<a href="https://echo.sintaks.web.id" target="_blank"><Rss size={16} /></a>
+		<a href="https://x.com/rnldnbbn" target="_blank">X</a>
+	</div>
+</section>
