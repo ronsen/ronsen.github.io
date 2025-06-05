f32fdc64285e13d0d0dac2f9d197a3edcf6a3948
Ronsen <ronsen@startrek.or.id>
2025-06-05 15:19:41 +0700

    update

---
+++ docs/_app/immutable/entry/start.DKoqnc_n.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DwYIf2uD.js";export{o as load_css,r as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
 	import {
+		Building2,
 		Code,
 		Database,
 		Frame,
+		Github,
 		Globe,
 		Mail,
 		MessageCircle,
 		Rss,
 		Smartphone,
 		SquareArrowOutUpRight,
+		Twitter,
 	} from "lucide-svelte";
 </script>
-
-<svelte:head>
-	<title>Ronald Nababan</title>
-</svelte:head>
 
 <section class="bg-sky-800 text-white text-center">
 	<a href="/"
@@ -31,151 +30,172 @@
 </section>
 
 <section>
-	<h3 class="section-title">Area of Expertise</h3>
-	<div class="grid md:grid-cols-2 gap-4">
-		<div class="card">
-			<div class="card-title">
-				<Globe />
-				<span>Web Programming</span>
+	<div class="section-container">
+		<h3 class="section-title">Area of Expertise</h3>
+		<div class="grid md:grid-cols-2 gap-4">
+			<div class="card">
+				<div class="card-title">
+					<Globe />
+					<span>Web Programming</span>
+				</div>
 			</div>
-		</div>
-		<div class="card">
-			<div class="card-title">
-				<Smartphone />
-				<span>Mobile Programming</span>
+			<div class="card">
+				<div class="card-title">
+					<Smartphone />
+					<span>Mobile Programming</span>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
 <section class="bg-zinc-50">
-	<h3 class="section-title">Technical Skills</h3>
-	<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-		<div class="card">
-			<div class="card-title">
-				<Code size={16} />
-				<span>Programming</span>
+	<div class="section-container">
+		<h3 class="section-title">Technical Skills</h3>
+		<div class="grid grid-cols-2 gap-4">
+			<div class="card">
+				<div class="card-title">
+					<Code size={16} />
+					<span>Programming</span>
+				</div>
+				<div class="card-content">
+					<ul>
+						<li>PHP</li>
+						<li>JavaScript</li>
+						<li>Python</li>
+						<li>Java</li>
+						<li>Dart</li>
+					</ul>
+				</div>
 			</div>
-			<div class="card-content">
-				<ul>
-					<li>PHP</li>
-					<li>JavaScript</li>
-					<li>Python</li>
-					<li>Java</li>
-					<li>Dart</li>
-				</ul>
+			<div class="card">
+				<div class="card-title">
+					<Frame size={16} />
+					<span>Frameworks</span>
+				</div>
+				<div class="card-content">
+					<ul>
+						<li>Laravel</li>
+						<li>Svelte/SvelteKit</li>
+						<li>Prisma</li>
+						<li>ExpressJS</li>
+						<li>Flutter</li>
+					</ul>
+				</div>
 			</div>
-		</div>
-		<div class="card">
-			<div class="card-title">
-				<Frame size={16} />
-				<span>Frameworks</span>
+			<div class="card">
+				<div class="card-title">
+					<Database size={16} />
+					<span>Databases</span>
+				</div>
+				<div class="card-content">
+					<ul>
+						<li>PostgreSQL</li>
+						<li>MariaDB/MySQL</li>
+						<li>sqlite</li>
+					</ul>
+				</div>
 			</div>
-			<div class="card-content">
-				<ul>
-					<li>Laravel</li>
-					<li>Svelte/SvelteKit</li>
-					<li>Prisma</li>
-					<li>ExpressJS</li>
-					<li>Flutter</li>
-				</ul>
-			</div>
-		</div>
-		<div class="card">
-			<div class="card-title">
-				<Database size={16} />
-				<span>Databases</span>
-			</div>
-			<div class="card-content">
-				<ul>
-					<li>PostgreSQL</li>
-					<li>MariaDB/MySQL</li>
-					<li>sqlite</li>
-				</ul>
-			</div>
-		</div>
-		<div class="card">
-			<div class="card-title">
-				<Smartphone size={16} />
-				<span>Tools</span>
-			</div>
-			<div class="card-content">
-				<ul>
-					<li>Linux</li>
-					<li>Docker</li>
-					<li>git</li>
-				</ul>
+			<div class="card">
+				<div class="card-title">
+					<Smartphone size={16} />
+					<span>Tools</span>
+				</div>
+				<div class="card-content">
+					<ul>
+						<li>Linux</li>
+						<li>Docker</li>
+						<li>git</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
 <section>
-	<h3 class="section-title">Projects</h3>
-	<div class="grid md:grid-cols-3 gap-4">
-		<div class="card">
-			<div class="flex justify-between items-center gap-2">
-				<div class="inline-flex items-center gap-1">
-					<img
-						src="https://www.hetanews.com/favicon.ico"
-						width="16px"
-						alt="[]"
-					/>
-					<span>Heta News</span>
+	<div class="section-container">
+		<h3 class="section-title">Projects</h3>
+		<div class="grid md:grid-cols-3 gap-4">
+			<div class="card">
+				<div class="flex justify-between items-center gap-2">
+					<div class="inline-flex items-center gap-1">
+						<img
+							src="https://www.hetanews.com/favicon.ico"
+							width="16px"
+							alt="[]"
+						/>
+						<span>Heta News</span>
+					</div>
+					<a href="https://www.hetanews.com" target="_blank"
+						><SquareArrowOutUpRight size={14} /></a
+					>
 				</div>
-				<a href="https://www.hetanews.com" target="_blank"
-					><SquareArrowOutUpRight size={14} /></a
-				>
 			</div>
-		</div>
-		<div class="card">
-			<div class="flex justify-between items-center gap-2">
-				<div class="inline-flex items-center gap-1">
-					<img src="https://fokus24.id/favicon.ico" width="16px" alt="[]" />
-					<span>Fokus24</span>
+			<div class="card">
+				<div class="flex justify-between items-center gap-2">
+					<div class="inline-flex items-center gap-1">
+						<img src="https://fokus24.id/favicon.ico" width="16px" alt="[]" />
+						<span>Fokus24</span>
+					</div>
+					<a href="https://fokus24.id" target="_blank"
+						><SquareArrowOutUpRight size={14} /></a
+					>
 				</div>
-				<a href="https://fokus24.id" target="_blank"
-					><SquareArrowOutUpRight size={14} /></a
-				>
 			</div>
-		</div>
-		<div class="card">
-			<div class="flex justify-between items-center gap-2">
-				<div class="inline-flex items-center gap-1">
-					<img src="https://kilasutama.com/favicon.ico" width="16px" alt="[]" />
-					<span>Kilas Utama</span>
+			<div class="card">
+				<div class="flex justify-between items-center gap-2">
+					<div class="inline-flex items-center gap-1">
+						<img
+							src="https://kilasutama.com/favicon.ico"
+							width="16px"
+							alt="[]"
+						/>
+						<span>Kilas Utama</span>
+					</div>
+					<a href="https://kilasutama.com" target="_blank"
+						><SquareArrowOutUpRight size={14} /></a
+					>
 				</div>
-				<a href="https://kilasutama.com" target="_blank"
-					><SquareArrowOutUpRight size={14} /></a
-				>
 			</div>
 		</div>
 	</div>
 </section>
 
 <section class="bg-zinc-50">
-	<h3 class="section-title">Let's Connect</h3>
-	<div class="text-center mb-6">
-		Do you have any questions regarding website and application development?
-		Let's discuss first!
-	</div>
-	<div class="flex justify-center items-center gap-4">
-		<a
-			href="mailto:ronsen@startrek.or.id"
-			class="btn btn-primary inline-flex items-center gap-2"
-			><Mail /><span>Email me</span></a
-		>
-		<a
-			href="mailto:ronsen@startrek.or.id"
-			class="btn btn-success inline-flex items-center gap-2"
-			><MessageCircle /><span>WhatsApp</span></a
-		>
+	<div class="section-container">
+		<h3 class="section-title">Let's Connect</h3>
+		<div class="text-center mb-6">
+			Do you have any questions regarding website and application development?
+			Let's discuss first!
+		</div>
+		<div class="flex justify-center items-center gap-4">
+			<a
+				href="mailto:ronsen@startrek.or.id"
+				class="btn btn-primary inline-flex items-center gap-2"
+				><Mail /><span>Email me</span></a
+			>
+			<a
+				href="mailto:ronsen@startrek.or.id"
+				class="btn btn-success inline-flex items-center gap-2"
+				><MessageCircle /><span>WhatsApp</span></a
+			>
+		</div>
 	</div>
 </section>
 
 <section class="bg-zinc-900 text-white">
-	<div class="flex justify-center items-center gap-2">
-		<a href="https://echo.sintaks.web.id" target="_blank"><Rss size={16} /></a>
-		<a href="https://x.com/rnldnbbn" target="_blank">X</a>
+	<div class="section-container">
+		<div class="flex justify-center items-center gap-4">
+			<a href="https://sintaks.web.id" target="_blank"
+				><Building2 size={18} /></a
+			>
+			<a href="https://echo.sintaks.web.id" target="_blank"><Rss size={18} /></a
+			>
+			<a href="https://github.com/ronsen" target="_blank"
+				><Github size={18} /></a
+			>
+			<a href="https://x.com/rnldnbbn" target="_blank"><Twitter size={18} /></a>
+		</div>
 	</div>
 </section>
